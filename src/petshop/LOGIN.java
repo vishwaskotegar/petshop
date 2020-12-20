6e9be7c4dffa3d41143ea6d14d2891e7c59ac0f3
vishwasskotegar <59993113+vishwasskotegar@users.noreply.github.com>
2020-12-20 13:46:39 +0530

    update

--- src/petshop/LOGIN.java
+++ src/petshop/LOGIN.java
@@ -9,17 +9,14 @@ class Login extends JFrame{
         setTitle("PET SHOP");
         setSize(500,700);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-        setLayout(new BorderLayout());
-        setContentPane(new JLabel(new ImageIcon("resources\\PicsArt_07-18-08.47.54")));
         setLayout(null);
+        setLocationRelativeTo(null);
 
 
-
-        JLabel login = new JLabel("LOGIN PAGE");
-        login.setBounds(160,200,200,25 );
-        login.setFont(new Font("Roboto",Font.BOLD,25));
-        login.setForeground(new Color(0X045FB4));
+        JLabel login = new JLabel("L O G I N    P A G E");
+        login.setBounds(110,200,300,25 );
+        login.setFont(new Font("Roboto",Font.BOLD,30));
+        login.setForeground(new Color(0x4581D4));
         add(login);
 
 
@@ -29,23 +26,41 @@ class Login extends JFrame{
 
         JTextField userField = new JTextField();
         userField.setBounds(150,350,200,25);
-        //userField.setForeground(Color.WHITE);
-        //userField.setBackground(new Color(10,120,140,85));
+        userField.setBorder(null);
+        userField.setOpaque(false);
         add(userField);
+
+        JSeparator userSeperator = new JSeparator();
+        userSeperator.setBounds(150,375,200,5);
+        add(userSeperator);
 
         JLabel password = new JLabel("PASSWORD ");
         password.setBounds(50,400,100,25 );
         add(password);
 
         JPasswordField passField = new JPasswordField();
+        passField.setOpaque(false);
         passField.setBounds(150,400,200,25);
-        //passField.setBackground(new Color(10,120,140,85));
+        passField.setBorder(null);
         add(passField);
 
+        JSeparator passSeperator = new JSeparator();
+        passSeperator.setBounds(150,425,200,5);
+        add(passSeperator);
+
         JButton logButton = new JButton("LOGIN ");
-        logButton.setBounds(150,450,200,25);
+        logButton.setBounds(150,500,200,25);
+        logButton.setFocusable(false);
+        //logButton.setBackground(new Color(0xBED0EA));
         add(logButton);
 
+        ImageIcon bkg = new ImageIcon("resources\\PicsArt_07-18-08.47.54.jpg");
+        JLabel background = new JLabel();
+        background.setBounds(0,0,500,700);
+        background.setIcon(bkg);
+        add(background);
+
+        setResizable(false);
         setVisible(true);
     }
 
