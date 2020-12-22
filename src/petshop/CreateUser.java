ee5aba2d9dfa0eeed75b9d3b92460d29b5badd56
vishwasskotegar <59993113+vishwasskotegar@users.noreply.github.com>
2020-12-22 10:28:40 +0530

    v1.2

---
+++ src/petshop/CreateUser.java
@@ -0,0 +1,18 @@
+package petshop;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+
+class CreateUser extends JFrame {
+    CreateUser() {
+        setTitle("PET SHOP");
+        setSize(500, 700);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLayout(null);
+        setUndecorated(true);
+        setLocationRelativeTo(null);
+
+        setVisible(true);
+    }
+}
--- src/petshop/LOGIN.java
+++ src/petshop/LOGIN.java
@@ -1,17 +1,25 @@
 package petshop;
 import java.awt.*;
 import java.awt.event.*;
+import java.sql.*;
 import javax.swing.*;
 
 
 class Login extends JFrame{
     Login(){
+
         setTitle("PET SHOP");
         setSize(500,700);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLayout(null);
         setUndecorated(true);
         setLocationRelativeTo(null);
+
+        JPanel panel = new MotionPanel(this);
+        panel.setBounds(0, 0, 500, 50);
+        panel.setOpaque(false);
+        panel.setLayout(null);
+        add(panel);
 
 
         JLabel login = new JLabel("L O G I N  ");
@@ -22,40 +30,110 @@ class Login extends JFrame{
 
 
         JLabel username = new JLabel("USERNAME ");
-        username.setBounds(25,450,150,25 );
+        username.setBounds(25,500,150,25 );
         username.setFont(new Font("Open Sans",Font.PLAIN,20));
         add(username);
 
         JTextField userField = new JTextField();
-        userField.setBounds(150,450,275,25);
+        userField.setBounds(155,500,275,25);
+        userField.setFont(new Font("",Font.PLAIN,20));
+        //userField.setForeground(new Color(77, 19, 209));
         userField.setBorder(null);
         userField.setOpaque(false);
         add(userField);
 
         JSeparator userSeperator = new JSeparator();
-        userSeperator.setBounds(150,475,275,5);
+        userSeperator.setBounds(150,525,275,5);
         add(userSeperator);
 
         JLabel password = new JLabel("PASSWORD ");
-        password.setBounds(25,500,150,25 );
+        password.setBounds(25,550,150,25 );
         password.setFont(new Font("Open Sans",Font.PLAIN,20));
         add(password);
 
         JPasswordField passField = new JPasswordField();
         passField.setOpaque(false);
-        passField.setBounds(150,500,275,25);
+        passField.setBounds(155,550,275,25);
+        passField.setFont(new Font("Open Sans",Font.PLAIN,20));
         passField.setBorder(null);
         add(passField);
 
         JSeparator passSeperator = new JSeparator();
-        passSeperator.setBounds(150,525,275,5);
+        passSeperator.setBounds(150,575,275,5);
         add(passSeperator);
 
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/petshop?useSSL=false","root","1234");
+
+            String mysqlUsername = username.getText();
+            String mysqlPassword = password.getText();
+
+            Statement stm = con.createStatement();
+
+            //String sql = "select * from employee where username = '"+mysqlUsername+"'"
+        }catch (Exception ce){
+            System.out.println(ce.getMessage());
+        }
+
         JButton logButton = new JButton("LOGIN ");
-        logButton.setBounds(150,600,200,25);
+        logButton.setBounds(150,585,275,50);
         logButton.setFocusable(false);
-        //logButton.setBackground(new Color(0xBED0EA));
+        logButton.setBackground(new Color(0x1C97A3));
+        logButton.setFont(new Font("Open Sans",Font.PLAIN,20));
+        logButton.addActionListener(e -> {});
         add(logButton);
+
+        JLabel newUser = new JLabel("CREATE NEW");
+        //newUser.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+        newUser.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                new CreateUser();
+                dispose();
+            }
+
+            @Override
+            public void mouseEntered(MouseEvent e){
+                newUser.setForeground(Color.WHITE.darker());
+            }
+
+            @Override
+            public void mouseExited(MouseEvent e){
+                newUser.setForeground(Color.BLACK.darker());
+            }
+        });
+        newUser.setBounds(350,655,200,25);
+        newUser.setFont(new Font("",Font.PLAIN,18));
+        add(newUser);
+
+        ImageIcon cancel = new ImageIcon("resources\\cancel.png");
+        JButton jcancel = new JButton();
+        jcancel.setBounds(450,0,50,50);
+        jcancel.setIcon(cancel);
+        jcancel.setOpaque(false);
+        jcancel.setContentAreaFilled(false);
+        jcancel.setBorderPainted(false);
+        jcancel.setFocusable(false);
+        jcancel.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent c) {
+
+            }
+
+            @Override
+            public void mouseEntered(MouseEvent c) {
+                ImageIcon cancel1 = new ImageIcon("resources\\cancel1.png");
+                jcancel.setIcon(cancel1);
+
+            }
+
+            @Override
+            public void mouseExited(MouseEvent c) {
+                jcancel.setIcon(cancel);
+            }
+        });
+        panel.add(jcancel);
 
         ImageIcon bkg = new ImageIcon("resources\\PicsArt_07-18-08.47.54.jpg");
         JLabel background = new JLabel();
@@ -68,6 +146,9 @@ class Login extends JFrame{
         jicon.setBounds(80,0,500,500);
         jicon.setIcon(icon);
         background.add(jicon);
+
+        jcancel.addActionListener(e -> System.exit(0));
+
 
         setResizable(false);
         setVisible(true);
